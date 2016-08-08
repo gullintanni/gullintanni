52e6a297cc4634685f7395771ea7dfa08a2bc5af
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-08-07 23:04:06 -0700

    Define a repository struct

---
+++ lib/gullintanni/repo.ex
@@ -0,0 +1,33 @@
+defmodule Gullintanni.Repo do
+  @moduledoc """
+  A reference to a Git repository.
+  """
+
+  alias __MODULE__, as: Repo
+
+  @typedoc "The repository type"
+  @type t ::
+    %Repo{
+      owner: String.t,
+      name: String.t
+    }
+
+  @enforce_keys [:owner, :name]
+  defstruct [:owner, :name]
+
+  @doc """
+  Creates a new repository with the given metadata.
+
+  ## Examples
+
+      iex> Gullintanni.Repo.new("elasticdog", "transcrypt")
+      %Gullintanni.Repo{name: "transcrypt", owner: "elasticdog"}
+  """
+  @spec new(String.t, String.t) :: t
+  def new(owner, name) do
+    %Repo{
+      owner: owner,
+      name: name
+    }
+  end
+end
