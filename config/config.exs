549810857e6586e00561e7457baf6c0a007a35e3
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-08-07 23:22:37 -0700

    Define an example pipeline configuration

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,12 @@ config :logger,
 
 config :logger, :console,
   format: "$date $time $metadata[$level] $levelpad$message\n"
+
+config :gullintanni, :pipeline,
+  my_project:
+    [
+      provider: Gullintanni.Providers.GitHub,
+      provider_auth_token: {:system, "GUL_PROVIDER_AUTH_TOKEN"},
+      repo_owner: {:system, "GUL_REPO_OWNER"},
+      repo_name: {:system, "GUL_REPO_NAME"}
+    ]
