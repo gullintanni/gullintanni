68c120cae296b54f3cef04ab33b7f1e3d7781931
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-08-07 23:18:13 -0700

    Allow configuration of the GitHub API endpoint URL
    Defaults to the regular (non-enterprise) API endpoint.

--- lib/gullintanni/providers/github.ex
+++ lib/gullintanni/providers/github.ex
@@ -1,6 +1,8 @@
 defmodule Gullintanni.Providers.GitHub do
   @behaviour Gullintanni.Provider
 
+  @default_endpoint "https://api.github.com/"
+
   def validate_config(config) do
     unless config[:provider_auth_token] do
       raise ArgumentError, "missing :provider_auth_token configuration"
@@ -14,6 +16,7 @@ defmodule Gullintanni.Providers.GitHub do
   end
 
   defp client(config) do
-    Tentacat.Client.new(%{access_token: config[:provider_auth_token]})
+    endpoint = config[:provider_endpoint] || @default_endpoint
+    Tentacat.Client.new(%{access_token: config[:provider_auth_token]}, endpoint)
   end
 end
