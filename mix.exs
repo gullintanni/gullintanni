31ebc6489bafed5bdf9551bc623a692ded4f1dc2
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-12-28 11:46:47 -0800

    Ensure that Coveralls can run from the umbrella app
    The *deploy-docs* script failed on Travis CI due to the missing
    specification in the umbrella project's *mix.exs* file. I'm not sure if
    Coveralls will handle an umbrella project properly, but we'll find out.
    
    ```
    ** (ExCoveralls.InvalidConfigError) Please specify 'test_coverage: [tool: ExCoveralls]' in the 'project' section of mix.exs
        lib/mix/tasks.ex:31: Mix.Tasks.Coveralls.do_run/2
        (mix) lib/mix/task.ex:296: Mix.Task.run_task/3
        (mix) lib/mix/cli.ex:58: Mix.CLI.run_task/2
        (elixir) lib/code.ex:363: Code.require_file/2
    ```

--- mix.exs
+++ mix.exs
@@ -16,21 +16,13 @@ defmodule Gullintanni.Platform.Mixfile do
             main: "readme",
             extras: ["README.md": [title: "README"],
                      "pages/Cheatsheet.md": [title: "Cheatsheet"],
-                     "CONTRIBUTING.md": [title: "Contributing"]]]]
+                     "CONTRIBUTING.md": [title: "Contributing"]]],
+
+     # Tests
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: [coveralls: :test]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
   end
